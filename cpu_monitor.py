e25e115bcb557146f130dd14d5580a34c94a4e93
Elliott Magnuson <ecmagnuson@wisc.edu>
2022-03-26 01:54:07 -0500

    added time

--- cpu_monitor.py
+++ cpu_monitor.py
@@ -3,11 +3,15 @@ import psutil
 cpu_count = psutil.cpu_count()
 
 with open('core_monitor.csv', 'w') as f:
+    f.write('time (s), ')
     for i in range(1, cpu_count + 1):
         f.write('core' + str(i) + ', ')
     f.write('\n') 
 
 with open('core_monitor.csv', 'a') as f:
+    i = 1
     while True:
         str_cpu = str(psutil.cpu_percent(interval=1, percpu=True))
+        f.write(str(i) + ', ')
         f.write(str_cpu[1:-1] + '\n')
+        i+=1
