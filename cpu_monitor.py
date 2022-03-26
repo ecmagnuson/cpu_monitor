f75467df8c69a173465fbd986522078e4714ec51
Elliott Magnuson <ecmagnuson@wisc.edu>
2022-03-26 01:33:52 -0500

    works

--- cpu_monitor.py
+++ cpu_monitor.py
@@ -1,4 +1,17 @@
 import pandas as pd
 import os
+import psutil
 
-var = os.system("cat /proc/cpuinfo | grep 'cpu MHz'")
+#var = os.system("/proc/cpuinfo | grep 'cpu MHz'")
+
+cpu_count = psutil.cpu_count()
+
+with open('core_monitor.csv', 'w') as f:
+    for i in range(1, cpu_count + 1):
+        f.write('core' + str(i) + ', ')
+    f.write('\n') 
+
+with open('core_monitor.csv', 'a') as f:
+    while True:
+        str_cpu = str(psutil.cpu_percent(interval=1, percpu=True))
+        f.write(str_cpu[1:-1] + '\n')
